6efa17be59d0af926c378aaa368de1bd2758557d
mpandagre25 <41897456+mpandagre25@users.noreply.github.com>
2022-03-24 18:23:25 +0530

    Update Game.cs

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +8,57 @@ namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        public bool isSpare(int frameIndex)
         {
-            // Add your logic here. Add classes as needed.
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
 
-        public int GetScore()
+        public bool isStrike(int frameIndex)
         {
-            // Returns the final score of the game.
-            return 0;
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
         }
     }
 }
